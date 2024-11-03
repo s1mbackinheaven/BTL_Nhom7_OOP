b9a08bc7675b0c9060cdb4c965b161529183448e
s1mbackinheaven <bachvochieu@gmail.com>
2024-11-03 22:09:54 +0700

    Test

--- src/main/java/com/example/BTL_Nhom7_OOP/BtlNhom7OopApplication.java
+++ src/main/java/com/example/BTL_Nhom7_OOP/BtlNhom7OopApplication.java
@@ -2,10 +2,16 @@ package com.example.BTL_Nhom7_OOP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BtlNhom7OopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BtlNhom7OopApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
 	}
 }
