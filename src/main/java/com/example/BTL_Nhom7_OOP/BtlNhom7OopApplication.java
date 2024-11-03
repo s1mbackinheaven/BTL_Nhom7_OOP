e97e4c775d03c434524ed9a821b55a9d9f47930a
s1mbackinheaven <bachvochieu@gmail.com>
2024-11-03 21:57:26 +0700

    Initial commit

---
+++ src/main/java/com/example/BTL_Nhom7_OOP/BtlNhom7OopApplication.java
@@ -0,0 +1,11 @@
+package com.example.BTL_Nhom7_OOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlNhom7OopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BtlNhom7OopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/BTL_Nhom7_OOP/BtlNhom7OopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTL_Nhom7_OOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlNhom7OopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
